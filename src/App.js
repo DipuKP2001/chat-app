9d988fd2a95d282d3035f5530412eebaa948dd83
DipuKP2001 <dipukp602@gmail.com>
2021-03-19 16:24:26 +0530

    .env setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import {ChatEngine} from 'react-chat-engine'
+
+const projectID = process.env.REACT_APP_API_KEY
+const userName = process.env.REACT_APP_USER_NAME
+const userSecret = process.env.REACT_APP_USER_SECRET
+
 const App = () => {
-    return(
-        <>
-        </>
+    return( 
+        <ChatEngine 
+            height="100vh" 
+            projectID={projectID}
+            userName={userName}
+            userSecret={userSecret}
+        />
     )
 }
 

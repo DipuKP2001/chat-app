cce9ae623a5c8c56701d40ebbaacdfa321c9ee68
DipuKP2001 <dipukp602@gmail.com>
2021-03-21 11:13:10 +0530

    TheirMessage complete

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,31 @@
-const TheirMessage = () => {
+const TheirMessage = ({ message,lastMessage }) => {
+    console.log(message)
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
     return(
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {
+                isFirstMessageByUser && (
+                    <div 
+                        className="message-avatar"
+                        style={{background:`url(${message?.sender?.avatar})`}} 
+                    />
+                )
+            }
+            {
+                message?.attachments?.length > 0
+                ? (
+                    <img 
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px':'48px' }}
+                    />
+                ):(
+                    <div className="message" style={{float:"left",backgroundColor:"#CABCDC"}}>
+                        {message.text}
+                    </div>  
+                )
+            }
         </div>
     )
 }

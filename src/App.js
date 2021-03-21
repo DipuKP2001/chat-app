d6e8d7eb2cad2c803732ec4d832f03ebaf3de453
DipuKP2001 <dipukp602@gmail.com>
2021-03-21 22:05:47 +0530

    LoginForm component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './App.css'
 
 import {ChatEngine} from 'react-chat-engine'
 
+import LoginForm from './components/LoginForm'
 import ChatFeed from './components/ChatFeed'
 
 const projectID = process.env.REACT_APP_API_KEY
-const userName = process.env.REACT_APP_USER_NAME
-const userSecret = process.env.REACT_APP_USER_SECRET
 
 const App = () => {
+
+    if(!localStorage.getItem('userName')) return <LoginForm />
+
     return(   
         <ChatEngine 
             height="100vh" 
             projectID={projectID}
-            userName={userName}
-            userSecret={userSecret}
+            userName={localStorage.getItem('userName')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
         />
     )
